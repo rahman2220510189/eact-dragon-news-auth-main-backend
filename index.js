132f384909d4390f191185e90fb404d79da8251b
rahman2220510189 <rahman22205101894@diu.edu.bd>
2026-03-19 23:19:20 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+dotenv.config();
+
+const app = express();
+app.use(cors({
+  origin: [
+    process.env.FRONTEND_URL,
+    "http://localhost:5173",
+  
+  ],
+  credentials: true,
+}));
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cjuyyb2.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    console.log("MongoDB Connected ");
+
+    // Routes define 
+    
+ 
+ 
+    // Test route
+    app.get("/", (req, res) => {
+      res.json({ message: "Career Coach API running ✅" });
+    });
+
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+
+  } catch (error) {
+    console.log("MongoDB Error ", error.message);
+  }
+}
+
+run().catch(console.dir);
